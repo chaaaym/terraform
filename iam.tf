dc3eb5772cb05ef3ed74f2e9500bf75cdc8d8cc9
chaaaym <109330253+chaaaym@users.noreply.github.com>
2022-09-30 17:35:23 +0900

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,321 @@
+resource "aws_iam_policy" "project_policy" {
+    name        = "project_policy"
+    path        = "/"
+    description = "policy for project"
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:*",
+                "organizations:DescribeAccount",
+                "organizations:DescribeOrganization",
+                "organizations:DescribeOrganizationalUnit",
+                "organizations:DescribePolicy",
+                "organizations:ListChildren",
+                "organizations:ListParents",
+                "organizations:ListPoliciesForTarget",
+                "organizations:ListRoots",
+                "organizations:ListPolicies",
+                "organizations:ListTargetsForPolicy"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": "*"
+        },
+        {
+            "Action": [
+                "dynamodb:*",
+                "dax:*",
+                "application-autoscaling:DeleteScalingPolicy",
+                "application-autoscaling:DeregisterScalableTarget",
+                "application-autoscaling:DescribeScalableTargets",
+                "application-autoscaling:DescribeScalingActivities",
+                "application-autoscaling:DescribeScalingPolicies",
+                "application-autoscaling:PutScalingPolicy",
+                "application-autoscaling:RegisterScalableTarget",
+                "cloudwatch:DeleteAlarms",
+                "cloudwatch:DescribeAlarmHistory",
+                "cloudwatch:DescribeAlarms",
+                "cloudwatch:DescribeAlarmsForMetric",
+                "cloudwatch:GetMetricStatistics",
+                "cloudwatch:ListMetrics",
+                "cloudwatch:PutMetricAlarm",
+                "cloudwatch:GetMetricData",
+                "datapipeline:ActivatePipeline",
+                "datapipeline:CreatePipeline",
+                "datapipeline:DeletePipeline",
+                "datapipeline:DescribeObjects",
+                "datapipeline:DescribePipelines",
+                "datapipeline:GetPipelineDefinition",
+                "datapipeline:ListPipelines",
+                "datapipeline:PutPipelineDefinition",
+                "datapipeline:QueryObjects",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeSecurityGroups",
+                "iam:GetRole",
+                "iam:ListRoles",
+                "kms:DescribeKey",
+                "kms:ListAliases",
+                "sns:CreateTopic",
+                "sns:DeleteTopic",
+                "sns:ListSubscriptions",
+                "sns:ListSubscriptionsByTopic",
+                "sns:ListTopics",
+                "sns:Subscribe",
+                "sns:Unsubscribe",
+                "sns:SetTopicAttributes",
+                "lambda:CreateFunction",
+                "lambda:ListFunctions",
+                "lambda:ListEventSourceMappings",
+                "lambda:CreateEventSourceMapping",
+                "lambda:DeleteEventSourceMapping",
+                "lambda:GetFunctionConfiguration",
+                "lambda:DeleteFunction",
+                "resource-groups:ListGroups",
+                "resource-groups:ListGroupResources",
+                "resource-groups:GetGroup",
+                "resource-groups:GetGroupQuery",
+                "resource-groups:DeleteGroup",
+                "resource-groups:CreateGroup",
+                "tag:GetResources",
+                "kinesis:ListStreams",
+                "kinesis:DescribeStream",
+                "kinesis:DescribeStreamSummary"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Action": "cloudwatch:GetInsightRuleReport",
+            "Effect": "Allow",
+            "Resource": "arn:aws:cloudwatch:*:*:insight-rule/DynamoDBContributorInsights*"
+        },
+        {
+            "Action": [
+                "iam:PassRole"
+            ],
+            "Effect": "Allow",
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "iam:PassedToService": [
+                        "application-autoscaling.amazonaws.com",
+                        "application-autoscaling.amazonaws.com.cn",
+                        "dax.amazonaws.com"
+                    ]
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateServiceLinkedRole"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": [
+                        "replication.dynamodb.amazonaws.com",
+                        "dax.amazonaws.com",
+                        "dynamodb.application-autoscaling.amazonaws.com",
+                        "contributorinsights.dynamodb.amazonaws.com",
+                        "kinesisreplication.dynamodb.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    {       
+            "Effect": "Allow",
+            "Action": [
+                "codepipeline:*",
+                "cloudformation:DescribeStacks",
+                "cloudformation:ListChangeSets",
+                "cloudtrail:DescribeTrails",
+                "codebuild:BatchGetProjects",
+                "codebuild:CreateProject",
+                "codebuild:ListCuratedEnvironmentImages",
+                "codebuild:ListProjects",
+                "codecommit:ListBranches",
+                "codecommit:GetReferences",
+                "codecommit:ListRepositories",
+                "codedeploy:BatchGetDeploymentGroups",
+                "codedeploy:ListApplications",
+                "codedeploy:ListDeploymentGroups",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeVpcs",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecs:ListClusters",
+                "ecs:ListServices",
+                "elasticbeanstalk:DescribeApplications",
+                "elasticbeanstalk:DescribeEnvironments",
+                "iam:ListRoles",
+                "iam:GetRole",
+                "lambda:ListFunctions",
+                "events:ListRules",
+                "events:ListTargetsByRule",
+                "events:DescribeRule",
+                "opsworks:DescribeApps",
+                "opsworks:DescribeLayers",
+                "opsworks:DescribeStacks",
+                "s3:ListAllMyBuckets",
+                "sns:ListTopics",
+                "codestar-notifications:ListNotificationRules",
+                "codestar-notifications:ListTargets",
+                "codestar-notifications:ListTagsforResource",
+                "codestar-notifications:ListEventTypes",
+                "states:ListStateMachines"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:GetBucketPolicy",
+                "s3:GetBucketVersioning",
+                "s3:GetObjectVersion",
+                "s3:CreateBucket",
+                "s3:PutBucketPolicy"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3::*:codepipeline-*"
+        },
+        {
+            "Action": [
+                "cloudtrail:PutEventSelectors",
+                "cloudtrail:CreateTrail",
+                "cloudtrail:GetEventSelectors",
+                "cloudtrail:StartLogging"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:cloudtrail:*:*:trail/codepipeline-source-trail"
+        },
+        {
+            "Action": [
+                "iam:PassRole"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:iam::*:role/service-role/cwe-role-*"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "iam:PassedToService": [
+                        "events.amazonaws.com"
+                    ]
+                }
+            }
+        },
+        {
+            "Action": [
+                "iam:PassRole"
+            ],
+            "Effect": "Allow",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:PassedToService": [
+                        "codepipeline.amazonaws.com"
+                    ]
+                }
+            }
+        },
+        {
+            "Action": [
+                "events:PutRule",
+                "events:PutTargets",
+                "events:DeleteRule",
+                "events:DisableRule",
+                "events:RemoveTargets"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:events:*:*:rule/codepipeline-*"
+            ]
+        },
+        {
+            "Sid": "CodeStarNotificationsReadWriteAccess",
+            "Effect": "Allow",
+            "Action": [
+                "codestar-notifications:CreateNotificationRule",
+                "codestar-notifications:DescribeNotificationRule",
+                "codestar-notifications:UpdateNotificationRule",
+                "codestar-notifications:DeleteNotificationRule",
+                "codestar-notifications:Subscribe",
+                "codestar-notifications:Unsubscribe"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "codestar-notifications:NotificationsForResource": "arn:aws:codepipeline:*"
+                }
+            }
+        },
+        {
+            "Sid": "CodeStarNotificationsSNSTopicCreateAccess",
+            "Effect": "Allow",
+            "Action": [
+                "sns:CreateTopic",
+                "sns:SetTopicAttributes"
+            ],
+            "Resource": "arn:aws:sns:*:*:codestar-notifications*"
+        },
+        {
+            "Sid": "CodeStarNotificationsChatbotAccess",
+            "Effect": "Allow",
+            "Action": [
+                "chatbot:DescribeSlackChannelConfigurations"
+            ],
+            "Resource": "*"
+        }
+    ]
+    })
+}
+
+resource "aws_iam_role" "project_role" {
+    name = "project_role"
+    path = "/"
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "project_role_attach" {
+    role       = aws_iam_role.project_role.name
+    policy_arn = aws_iam_policy.project_policy.arn
+}
+
+resource "aws_iam_instance_profile" "project_role_profile" {
+    name = "project_role__profile"
+    role = aws_iam_role.project_role.name
+}
+
+resource "aws_iam_user" "project_user" {
+  name = "project_user"
+  path = "/"
+}
+
+resource "aws_iam_user_policy_attachment" "project_user_attach" {
+  user       = aws_iam_user.project_user.name
+  policy_arn = aws_iam_policy.project_policy.arn
+}
