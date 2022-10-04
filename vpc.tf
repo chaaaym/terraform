14c6b3a9b889b3695c2f3714fb4224a947d2af42
chaaaym <109330253+chaaaym@users.noreply.github.com>
2022-10-04 17:31:27 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -74,3 +74,73 @@ resource "aws_route_table_association" "project-bastion-rt" {
   subnet_id      = aws_subnet.project-bastion.id
   route_table_id = aws_route_table.project-bastion-rt.id
 }
+
+# Public Security Group 생성
+resource "aws_security_group" "project-vpc-pub-sg" {
+  vpc_id = aws_vpc.project-vpc.id
+  name = "project-vpc-pub-sg"
+  description = "project-vpc-pub-sg"
+  tags = {
+    Name = "public SG"
+  }
+}
+
+## Public Security Group inbound rule
+resource "aws_security_group_rule" "HTTP80" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "TCP"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.project-vpc-pub-sg.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "SSH" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "TCP"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.project-vpc-pub-sg.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "HTTP8080" {
+  type = "ingress"
+  from_port = 8080
+  to_port = 8080
+  protocol = "TCP"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.project-vpc-pub-sg.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Private Security Group
+resource "aws_security_group" "project-vpc-pri-sg" {
+  vpc_id = aws_vpc.project-vpc.id
+  name = "project-vpc-pri-sg"
+  description = "project-vpc-pri-sg"
+  tags = {
+    Name = "private SG"
+  }
+}
+
+## Private Security Group inbound rules
+resource "aws_security_group_rule" "DB" {
+  type = "ingress"
+  from_port = 3306
+  to_port = 3306
+  protocol = "TCP"
+  security_group_id = aws_security_group.project-vpc-pri-sg.id
+  source_security_group_id = aws_security_group.project-vpc-pri-sg.id
+  lifecycle {
+    create_before_destroy = true
+  }
+}
